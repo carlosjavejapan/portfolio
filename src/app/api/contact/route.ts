e43c3e3faa8bf4c9f08b6274ea58b68364ff216b
carlosjavejapan <javedavalos@gmail.com>
2024-08-02 23:34:43 +0900

    nodemailer change port

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -8,7 +8,7 @@ export async function POST(request: NextRequest) {
 
   const transport = nodemailer.createTransport({
     service: "gmail",
-    port: 465,
+    port: 587,
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_PASS,
